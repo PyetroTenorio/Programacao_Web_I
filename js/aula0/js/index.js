6681eda55650ca7b196cd5f9394eb65dcb9f66db
PyetroTenorio <pyetrod88@gmail.com>
2026-09-23 15:26:43 -0300

    é isso

---
+++ js/aula0/js/index.js
@@ -0,0 +1,34 @@
+// comentario um linha
+/*
+comentario em bloco
+*/
+
+
+/*
+
+alert interação pop op
+window.alert("zamora is gay");
+*/
+
+
+
+// console.log(" Hello Guys");
+
+// let nome = window.prompt("whats your name?");
+// console.log("hello" + nome);
+
+/*
+let aluno = prompt("whats your name");
+let nota1 = prompt("Insira a nota1");
+let nota2 = prompt("Insira a nota2");
+let nota3 = prompt("Insira a nota3");
+
+let media = (nota1 + nota2 + nota3)/3
+
+console.log("o nome é " + aluno + "a media é " + media;)
+*/
+
+let num1 = Number(prompt("Insira o numero 1");)
+let num2 = Number(prompt("Insira o numero 2");)
+let soma = num1 + num2
+
---
+++ js/aula1-tiposvariaveis/js/index1.js
@@ -0,0 +1,27 @@
+// comentario um linha
+/*
+comentario em bloco
+*/
+
+
+/*
+
+alert interação pop op
+window.alert("zamora is gay");
+*/
+
+let num1 = 10; //tipo number
+let num2 = 7.5; //tipo number
+let nome = "gilmar"; //tipo string
+let sobrenome = "freire"; //tipo string
+let maioridade = true // tipo boolean
+let resposta; // 
+let saldo = null // tipo nulo
+
+console.log(typeof num1);
+console.log(typeof num2);
+console.log(typeof sobrenome);
+console.log(typeof maioridade);
+console.log(typeof resposta);
+console.log(typeof saldo);
+console.log(typeof nome);
